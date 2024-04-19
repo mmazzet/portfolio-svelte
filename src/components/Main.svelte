b9d7422672b90476444214f765bbe493c2ada6af
Marianna <20104120@mail.wit.ie>
2024-04-19 21:49:34 +0100

    added button and more text

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -6,8 +6,19 @@
     <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi! I'm <span class="poppins text-violet-400"> Name </span>Surname <br/>Full Stack <span class="poppins text-violet-400">Developer</span>
+                Hi! I'm <span class="poppins text-violet-400">Name</span> Surname <br/>Full Stack <span class="poppins text-violet-400">Developer</span>
             </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-violet-400">favourite tech</span> includes JavaScript (NEXT.JS or SvelteKit), TailwindCSS, Node.js + Express.JS & PostGresSQL or Firebase
+            </p>
+            <button
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+            >
+                <div
+                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                />
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+            </button>
         </div>
     </section>
 </main>
